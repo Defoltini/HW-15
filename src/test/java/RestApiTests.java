d67f6e02df7b2958b8bd1fb383b0d6666793717b
Defoltini <nesti-1998@mail.ru>
2025-03-06 01:24:21 +0300

    added lombok

--- src/test/java/RestApiTests.java
+++ src/test/java/RestApiTests.java
@@ -1,8 +1,12 @@
 import static io.restassured.RestAssured.*;
 import static io.restassured.http.ContentType.JSON;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import models.lombok.LombokModelAuthDataRequest;
+import models.lombok.LombokModelAuthDataResponse;
+import models.lombok.LombokModelJobData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,33 +25,39 @@ public class RestApiTests extends TestBase {
     @DisplayName("Проверка создания пользователя")
     @Test
     void createUser() {
-        String nameData = "{\"name\": \"morpheus\",\"job\": \"leader\"}";
+        LombokModelJobData jobData = new LombokModelJobData();
+        jobData.setName("morpheus");
+        jobData.setJob("leader");
         given()
-                .body(nameData)
+                .body(jobData)
                 .contentType(JSON)
                 .when()
                 .post("/users")
                 .then()
                 .log().all()
                 .statusCode(201)
-                .body("name", is("morpheus"))
-                .body("job", is("leader"));
+                .extract().as(LombokModelJobData.class);
+        assertEquals("morpheus", jobData.getName());
+        assertEquals("leader", jobData.getJob());
     }
 
     @DisplayName("Проверка обновления данных пользователя")
     @Test
     void updateUser() {
-        String authData = "{\"name\": \"morpheus\",\"job\": \"zion resident\"}";
+        LombokModelJobData jobData = new LombokModelJobData();
+        jobData.setName("morpheus");
+        jobData.setJob("zion resident");
         given()
-                .body(authData)
+                .body(jobData)
                 .contentType(JSON)
                 .when()
                 .put("/users/2")
                 .then()
                 .log().all()
                 .statusCode(200)
-                .body("name", is("morpheus"))
-                .body("job", is("zion resident"));
+                .extract().as(LombokModelJobData.class);
+        assertEquals("morpheus", jobData.getName());
+        assertEquals("zion resident", jobData.getJob());
     }
 
     @DisplayName("Удаление пользователя")
@@ -62,32 +72,43 @@ public class RestApiTests extends TestBase {
     @DisplayName("Проверка успешной регистрации пользователя")
     @Test
     void registrationUser() {
-        String authData = "{\"email\": \"eve.holt@reqres.in\",\"password\": \"pistol\"}";
-        given()
-                .body(authData)
-                .contentType(JSON)
-                .when()
-                .post("/register")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .body("id", is(4))
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+        LombokModelAuthDataRequest authData = new LombokModelAuthDataRequest();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("pistol");
+        LombokModelAuthDataResponse responseData =
+                given()
+                        .body(authData)
+                        .contentType(JSON)
+                        .when()
+                        .post("/register")
+                        .then()
+                        .log().all()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().as(LombokModelAuthDataResponse.class);
+        assertEquals("QpwL5tke4Pnpja7X4", responseData.getToken());
+        assertEquals(4, responseData.getId());
     }
+
 
     @DisplayName("Проверка успешной авторизации пользователя")
     @Test
     void authorizationUser() {
-        String authUser = "{\"email\": \"eve.holt@reqres.in\",\"password\": \"cityslicka\"}";
-        given()
-                .body(authUser)
-                .contentType(JSON)
-                .when()
-                .post("/login")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+        LombokModelAuthDataRequest authData = new LombokModelAuthDataRequest();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("pistol");
+        LombokModelAuthDataResponse responseData =
+                given()
+                        .body(authData)
+                        .contentType(JSON)
+                        .when()
+                        .post("/login")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .extract().as(LombokModelAuthDataResponse.class);
+        assertEquals("QpwL5tke4Pnpja7X4", responseData.getToken());
+
     }
 
 }
